2f4a43da7c06f32429a6cbd871bba06a2e5a07e9
Choo <choo000407@naver.com>
2023-12-14 15:25:24 +0900

    feat(Station): 이름(PK, UK)으로 역을 생성한다

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -5,11 +5,16 @@ public class Station {
 
     public Station(String name) {
         this.name = name;
+        validateName();
+    }
+
+    private void validateName() {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException("역 이름은 2글자 이상이어야 합니다.");
+        }
     }
 
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
