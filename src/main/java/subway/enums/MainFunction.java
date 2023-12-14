bf2fa3440e52c7077c6a73b3f674244828f99953
Choo <choo000407@naver.com>
2023-12-14 17:51:55 +0900

    feat(MainFunction): 메인 기능을 정의한다

--- src/main/java/subway/enums/MainFunction.java
+++ src/main/java/subway/enums/MainFunction.java
@@ -1,4 +1,31 @@
 package subway.enums;
 
 public enum MainFunction {
+    STATION_MANAGEMENT("1", "역 관리"),
+    LINE_MANAGEMENT("2", "노선 관리"),
+    SECTION_MANAGEMENT("3", "구간 관리"),
+    PRINT_SUBWAY_MAP("4", "지하철 노선도 출력"),
+    QUIT("Q", "종료");
+
+    private final String number;
+    private final String description;
+
+    MainFunction(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return number + ". " + description;
+    }
 }
+
--- src/main/java/subway/enums/SubFunction.java
+++ src/main/java/subway/enums/SubFunction.java
@@ -1,4 +1,30 @@
 package subway.enums;
 
 public enum SubFunction {
+    //## 역 관리 화면
+    //1. 역 등록
+    //2. 역 삭제
+    //3. 역 조회
+    //B. 돌아가기
+
+    //## 노선 관리 화면
+    //1. 노선 등록
+    //2. 노선 삭제
+    //3. 노선 조회
+    //B. 돌아가기
+
+    //## 구간 관리 화면
+    //1. 구간 등록
+    //2. 구간 삭제
+    //B. 돌아가기
+
+//    private final MainFunction mainFunction;
+//    private final String number;
+//    private final String description;
+//
+//    SubFunction(MainFunction mainFunction, String number, String description) {
+//        this.mainFunction = mainFunction;
+//        this.number = number;
+//        this.description = description;
+//    }
 }
