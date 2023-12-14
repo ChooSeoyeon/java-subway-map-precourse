3bcbcf05b89c5b4645eae6829d22a95e839cd40f
Choo <choo000407@naver.com>
2023-12-14 15:28:36 +0900

    feat(Registration): 노선(PK, UK)과 역 목록으로 등록을 생성한다

--- src/main/java/subway/domain/Registration.java
+++ src/main/java/subway/domain/Registration.java
@@ -1,4 +1,20 @@
 package subway.domain;
 
+import java.util.List;
+
 public class Registration {
+    private Line line;
+    private List<Station> stations;
+
+    public Registration(Line line, List<Station> stations) {
+        this.line = line;
+        this.stations = stations;
+        validateStations();
+    }
+
+    private void validateStations() {
+        if (stations.size() < 2) {
+            throw new IllegalArgumentException("노선에는 2개 이상의 역이 등록되어야 합니다.");
+        }
+    }
 }
