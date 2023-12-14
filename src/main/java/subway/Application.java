1ee8995c3c090fbe1875f7bdade3cf1920a0c507
Choo <choo000407@naver.com>
2023-12-14 15:21:57 +0900

    feat(Controller): 에러를 관리한다

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,13 @@
 package subway;
 
 import java.util.Scanner;
+import subway.controller.SubwayMapController;
+import subway.view.InputView;
+import subway.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        new SubwayMapController(new InputView(scanner), new OutputView()).run();
     }
 }
--- src/main/java/subway/controller/SubwayMapController.java
+++ src/main/java/subway/controller/SubwayMapController.java
@@ -1,4 +1,40 @@
 package subway.controller;
 
+import java.util.function.Supplier;
+import subway.view.InputView;
+import subway.view.OutputView;
+
 public class SubwayMapController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public SubwayMapController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        // TODO
+    }
+
+    private <T> T repeatUntilSuccessWithReturn(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void repeatUntilSuccess(Runnable action) {
+        while (true) {
+            try {
+                action.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }
